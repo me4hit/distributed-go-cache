285adf59b88c086497249c5971500eb42b31a6c5
me4hit <nghiahuawei10101999@gmail.com>
2024-11-22 01:58:39 +0700

    add main function to demonstrate LRU cache usage

---
+++ cmd/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/me4hit/distributed-go-cache/pkg/cache"
+)
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	lru := cache.NewLRUCache(2)
+
+	lru.Set("key1", "value1")
+
+	lru.Set("key2", "value2")
+
+	if value, ok := lru.Get("key1"); ok {
+		fmt.Println("key1:", value)
+	} else {
+		fmt.Println("key1 not found")
+	}
+
+	lru.Set("key3", "value3")
+
+	if value, ok := lru.Get("key2"); ok {
+		fmt.Println("key2:", value)
+	} else {
+		fmt.Println("key2 not found")
+	}
+
+	if value, ok := lru.Get("key3"); ok {
+		fmt.Println("key3:", value)
+	} else {
+		fmt.Println("key3 not found")
+	}
+}
